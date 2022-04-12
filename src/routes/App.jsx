47f90f948e6f9cfe91dad1e3101b8d64d7413437
RodrigoMoreno <rroderickk@gmail.com>
2022-04-12 08:15:33 -0500

    test1

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "@containers/Layout";
-import { Home 	} from "@pages/Home";
+import { Home } from "@pages/Home";
 import { NotFound } from "@pages/NotFound";
 
 import { GlobalStyle } from "../GlobalStyle"
@@ -9,28 +9,29 @@ import "../_vars.scss"//(@)=>
 
 import ThemeContext from "../components/ThemeContext";
 
-const App =()=> { 
+const App = () => {
 
-	const [light,setDark] = React.useState(false);
-  const color = React.useContext(ThemeContext);
+	const [light, setDark] = React.useState(false);
+	const color = React.useContext(ThemeContext);
 
-return (
-	<BrowserRouter basename="/htbmachines.github.io">
-	<GlobalStyle/>
-		<Layout light={light} 
-			setDark={setDark} 
-			color={color}
-		>
-			<Routes>
-				 <Route exact path="/" 
-					element={
-						<Home light={light} 
-							color={color} 
-						/>} 
-				 />
+	return (
+		<BrowserRouter basename="/htbmachines.github.io">
+			<GlobalStyle />
+			<Layout light={light}
+				setDark={setDark}
+				color={color}
+			>
+				<Routes>
+					<Route exact path="/"
+						element={
+							<Home light={light}
+								color={color}
+							/>}
+					/>
 
-				<Route exact path="*" element={<NotFound />} />
-			</Routes>
-		</Layout>
-	</BrowserRouter>
-); }; export { App };
+					<Route exact path="*" element={<NotFound />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
+}; export { App };
